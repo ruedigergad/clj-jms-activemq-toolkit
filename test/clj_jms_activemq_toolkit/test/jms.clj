036d7c413b389a539ad78be8e8b9e7836d05b933
Ruediger Gad <r.c.g@gmx.de>
2015-04-09 21:10:40 +0200

    Use IP address instead of host name.

--- test/clj_jms_activemq_toolkit/test/jms.clj
+++ test/clj_jms_activemq_toolkit/test/jms.clj
@@ -14,7 +14,7 @@
         clj-assorted-utils.util
         clj-jms-activemq-toolkit.jms))
 
-(def local-jms-server "tcp://localhost:42424")
+(def local-jms-server "tcp://127.0.0.1:42424")
 (def test-topic "/topic/testtopic.foo")
 
 (defn jms-broker-fixture [f]
