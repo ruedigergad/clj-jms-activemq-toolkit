f68b6061d79379dfb04a84aa970f8fa737d19a0e
Ruediger Gad <r.c.g@gmx.de>
2015-04-12 11:37:33 +0200

    Add test for client auth with stomp+ssl.

--- test/clj_jms_activemq_toolkit/test/jms_test_base.clj
+++ test/clj_jms_activemq_toolkit/test/jms_test_base.clj
@@ -32,6 +32,10 @@
             *trust-store-file* "test/ssl/client.ts"
             *key-store-file* "test/ssl/client.ks"]
     (run-test t))
+  (binding [*local-jms-server* "stomp+ssl://127.0.0.1:42423?needClientAuth=true"
+            *trust-store-file* "test/ssl/client.ts"
+            *key-store-file* "test/ssl/client.ks"]
+    (run-test t))
   (binding [*local-jms-server* "ssl://127.0.0.1:42425"
             *trust-store-file* "test/ssl/client.ts"
             *key-store-file* "test/ssl/client.ks"]
